926a60793b023a36665875947dea0cc6d83288bc
mayvu37 <124767564+mayvu37@users.noreply.github.com>
2026-03-07 22:25:07 -0800

    gait signature visualization and feature comparison
    all commented, add to functions later

--- WiFi/spec_2.py
+++ WiFi/spec_2.py
@@ -3,6 +3,9 @@ import spec_functions as sf
 import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import seaborn as sns
 
 class SpectrogramGenerator:
     def __init__(self, CSI_data):
@@ -36,7 +39,8 @@ class SpectrogramGenerator:
 # raw_CSI_data must be loaded from .mat file
 # path = r"c:/Users/ptv57/Downloads/data_500.mat"
 # path = r"c:/Users/ptv57/Downloads/xmodal_data_2000.mat"
-path = r"c:/Users/ptv57/Downloads/jose_walking_1_500Hz.mat"
+path = r"c:/Users/ptv57/Downloads/Jose_500_T1.mat"
+filename = os.path.splitext(os.path.basename(path))[0]
 
 mat = sio.loadmat(path)
 keys = [k for k in mat.keys() if not k.startswith("__")]
@@ -57,9 +61,6 @@ csi_post_pca = spec.prep_CSI_data(raw_CSI_data)
 f, t, mag = sf.compute_STFT(csi_post_pca, spec.fs, T_win=0.4)
 STFT_data, f = sf.process_stft_results(mag, f)
 
-
-
-
 sf.plot_spectrogram(f, t, STFT_data,
                  v_min = 0.3,
                  v_max = 2.25,
@@ -69,3 +70,74 @@ sf.plot_spectrogram(f, t, STFT_data,
 
                  figsize=(14,6))
 
+# t_min = 2.4
+# t_max = 8
+# time_mask = (t >= t_min) & (t <= t_max)
+
+# STFT_data = STFT_data[:, time_mask]
+# t = t[time_mask]
+
+
+# feature_vector = sf.generate_feature_vector(STFT_data, t, f)
+# sf.update_feature_file(feature_vector, filename)
+
+
+# file = "training_feature_vectors.xlsx"
+
+# df = pd.read_excel(file)
+
+# print(df.head())
+# print(df.columns)
+
+# plt.figure(figsize=(8,6))
+
+# sns.scatterplot(
+#     data=df,
+#     x="torso_avg",
+#     y="cycle_time",
+#     hue="Label",
+#     palette="tab10",
+#     s=80
+# )
+
+# plt.title("Feature Comparison: Torso Avg vs Stride Length")
+# plt.xlabel("Torso Avg Velocity")
+# plt.ylabel("Cycle Time")
+# plt.legend(title="Person")
+# plt.grid(True)
+
+# plt.show()
+
+
+# velocity_bins = [c for c in df.columns if "velocity_bin" in c]
+# labels = df["Label"].unique()
+
+# # iterate through labels in pairs
+# for i in range(0, len(labels), 2):
+
+#     fig, axes = plt.subplots(1, 2, figsize=(12,4))
+
+#     for j in range(2):
+
+#         if i + j >= len(labels):
+#             break
+
+#         label = labels[i + j]
+#         ax = axes[j]
+
+#         subset = df[df["Label"] == label]
+
+#         for _, row in subset.iterrows():
+
+#             y = row[velocity_bins].values.astype(float)
+#             x = np.arange(len(y))
+
+#             ax.plot(x, y, alpha=0.6)
+
+#         ax.set_title(f"Velocity Distribution: {label}")
+#         ax.set_xlabel("Velocity Bin")
+#         ax.set_ylabel("Energy")
+#         ax.grid(True)
+
+#     plt.tight_layout()
+#     plt.show()
